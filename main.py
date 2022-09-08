d7b46ea8f498c5f42adeef44bff1cc7b858479f4
PhoenixFire <72724975+help-programming-is-hard@users.noreply.github.com>
2022-09-07 19:51:10 -0700

    added bypass keyword in int_input function

--- main.py
+++ main.py
@@ -4,17 +4,26 @@ import datetime
 def clear():
   os.system('clear' if os.name == 'posix' else 'cls')
 
-def int_input(message, range1 = "null", range2 = "null"):
+def bold(message):
+  return "\033[1m" + message + "\033[0m"
+
+def int_input(message, range1 = "nil", range2 = "nil", bpsMessage = "nil"):
   while True:
     try:
-      output = int(input(str(message)))
+      output_raw = input(str(message))
+      
+      if bpsMessage != "nil":
+        if output_raw == bpsMessage:
+          return bpsMessage
 
-      if range1 != "null":
+      output = int(output_raw)
+
+      if range1 != "nil":
         if output < int(range1):
           print("Invalid value, please try again.\n")
           continue
          
-      if range2 != "null":
+      if range2 != "nil":
         if output > range2:
           print("Invalid value, please try again.\n")
           continue
@@ -40,23 +49,40 @@ def bool_input(message, trueString = "y", falseString = "n"):
       continue
 # =====================================================================
 validList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 25, 29, 30, 34, 36, 39, 48]
+defunctList = [23, 24]
+
+clear()
 
 while True:
   print("Numbers are indexed by their order on the Project Euler page.")
+  print("Type 'exit' to exit the program.")
   print("")
-  print("Valid inputs: {}".format(validList))
-  print("Extension Count: {}".format(len(validList)))
+  print("{} Valid inputs: {}".format(len(validList), validList))
+  print("{} Defunct inputs: {}".format(len(defunctList), defunctList))
+  print("Total Extension Count: {}".format(len(validList) + len(defunctList)))
   print("")
-  run = int_input("Input the extension number to run here: ", 0)
+  
+  run = int_input("Input the extension number to run here: ", 0, "nil", "exit")
+  if run == "exit":
+    break
+  
+  if run in defunctList:
+    print("This problem is defunct/work in progress, please choose another.")
+    print("-----------------------------------------------------")
+  elif run in validList:
+    runString = str(run) + ".py"
 
-  runString = str(run) + ".py"
+    print("")
+    timeStart = datetime.datetime.now()
+    exec(open(runString).read())
+    timeEnd = datetime.datetime.now()
 
-  print("")
-  timeStart = datetime.datetime.now()
-  exec(open(runString).read())
-  timeEnd = datetime.datetime.now() 
-  print("-----------------------------------------------------")
-  print("Execution Time was " + str(timeEnd - timeStart))
+    print("-----------------------------------------------------")
+    print("Execution Time was " + str(timeEnd - timeStart))
+  else:
+    print("Invalid input, please try again.")
+    print("-----------------------------------------------------")
+
   print("")
   input("Press enter to continue.")
   clear()
